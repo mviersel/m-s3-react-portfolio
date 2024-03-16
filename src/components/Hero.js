1f456a564c8b0f5c4d986c698eec8a7c2a42f48d
mviersel <119597405+mviersel@users.noreply.github.com>
2024-03-16 16:54:38 +0100

    Aanmaken react app

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='hero-img-div'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
